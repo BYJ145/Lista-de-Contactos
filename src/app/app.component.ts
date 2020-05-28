7e3354c98c585c9d0608e63f960ad48bfb30bd2e
Jose Gomez <GomezLopezJose145@gmail.com>
2020-05-27 19:50:10 -0500

    Se agrega th a tabla y se agrega funcionamiento del metodo editEmploye

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,16 +17,26 @@ export class AppComponent {
   // Modelos que servira para actualizar los datos (databinding)
   model : any = {};
   model2 : any = {};
+  // Se agrega atrituto para almacenar el index
+  myvalue;
   // Se agrega metodos para le manejo de los datos
+
   addEmployee():void{
     // Guardaremos los datos introducidos a travez del formulario
     this.employees.push(this.model);
   }
-  deleteEmployee(i):void{
+  deleteEmployee():void{
   }
   editEmployee(i):void{
+    // Se "prepara" los datos para ser actualizados
+    this.model2.name = this.employees[i].name;
+    this.model2.position = this.employees[i].position;
+    this.model2.email = this.employees[i].email;
+    // i = indice el arreglo
+    this.myvalue = i;
   }
   updateEmployee(){
+    
   }
 
 }
